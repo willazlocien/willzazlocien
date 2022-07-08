fde5ea41b4f978a8bcb39f5d783a96315e44dd37
kjuba21 <kjuba21@gmail.com>
2022-07-08 16:06:01 +0200

    123

--- src/components/Standart.jsx
+++ src/components/Standart.jsx
@@ -1,6 +1,6 @@
 import "materialize-css";
 import React, { Component, useEffect } from "react";
-
+import { Link } from "react-router-dom";
 
 
 
@@ -10,7 +10,30 @@ const Standart = () => {
 
 
   return (
-<div className="home"></div>
+<div className="home">
+  <div className="hide-on-med-and-up">
+
+ 
+  <br/><br/>
+<Link to="home" className="btn red navbaritem">
+            O inwestycji
+          </Link>
+<br/><br/>
+          <Link to="standart" className="btn red navbaritem">
+            Standart
+          </Link>
+          <br/><br/>
+          <Link to="offer" className="btn red navbaritem" >
+            Lokale
+          </Link><br/><br/>
+          <Link to="gallery" className="btn red navbaritem">
+            Galeria
+          </Link><br/><br/>
+          <Link to="contact" className="btn red navbaritem">
+            Kontakt
+          </Link>
+ </div>
+</div>
 
   );
 };
