f3ab408a5e58f9761b42346ea044cb6e33ae053f
kjuba21 <kjuba21@gmail.com>
2023-12-06 12:03:38 +0100

    123

--- src/components/Pawla12.jsx
+++ src/components/Pawla12.jsx
@@ -1,9 +1,8 @@
 import db from "./firebase.config";
 import React, { useState, useEffect } from "react";
 
-
 import "materialize-css";
-import { Divider,  Button, Modal,Col, Row } from "react-materialize";
+import { Divider, Button, Modal, Col, Row } from "react-materialize";
 import lokalm01 from "./svg/M01.svg";
 import lokalm02 from "./svg/M02.svg";
 import lokalm03 from "./svg/M03.svg";
@@ -38,60 +37,57 @@ import lokalm35 from "./svg/M35.svg";
 import lokalm36 from "./svg/M36.svg";
 import lokalm37 from "./svg/M37.svg";
 
-
 const Lista = [
-
-  {id:"lokalm01", lokal: lokalm01, active: ""},
-  {id:"lokalm02", lokal: lokalm02, active: ""},
-  {id:"lokalm03", lokal: lokalm03, active: ""},
-  {id:"lokalm04", lokal: lokalm04, active: ""},
-  {id:"lokalm05", lokal: lokalm05, active: ""},
-  {id:"lokalm06", lokal: lokalm06, active: ""},
-  {id:"lokalm07", lokal: lokalm07, active: ""},
-  {id:"lokalm08", lokal: lokalm08, active: ""},
-  {id:"lokalm11", lokal: lokalm11, active: ""},
-  {id:"lokalm12", lokal: lokalm12, active: ""},
-  {id:"lokalm13", lokal: lokalm13, active: ""},
-  {id:"lokalm14", lokal: lokalm14, active: ""},
-  {id:"lokalm15", lokal: lokalm15, active: ""},
-  {id:"lokalm16", lokal: lokalm16, active: ""},
-  {id:"lokalm17", lokal: lokalm17, active: ""},
-  {id:"lokalm18", lokal: lokalm18, active: ""},
-  {id:"lokalm19", lokal: lokalm19, active: ""},
-  {id:"lokalm21", lokal: lokalm21, active: ""},
-  {id:"lokalm22", lokal: lokalm22, active: ""},
-  {id:"lokalm23", lokal: lokalm23, active: ""},
-  {id:"lokalm24", lokal: lokalm24, active: ""},
-  {id:"lokalm25", lokal: lokalm25, active: ""},
-  {id:"lokalm26", lokal: lokalm26, active: ""},
-  {id:"lokalm27", lokal: lokalm27, active: ""},
-  {id:"lokalm28", lokal: lokalm28, active: ""},
-  {id:"lokalm29", lokal: lokalm29, active: ""},
-  {id:"lokalm31", lokal: lokalm31, active: ""},
-  {id:"lokalm32", lokal: lokalm32, active: ""},
-  {id:"lokalm33", lokal: lokalm33, active: ""},
-  {id:"lokalm34", lokal: lokalm34, active: ""},
-  {id:"lokalm35", lokal: lokalm35, active: ""},
-  {id:"lokalm36", lokal: lokalm36, active: ""},
-  {id:"lokalm37", lokal: lokalm37, active: ""},
-  ];
+  { id: "lokalm01", lokal: lokalm01, active: "" },
+  { id: "lokalm02", lokal: lokalm02, active: "" },
+  { id: "lokalm03", lokal: lokalm03, active: "" },
+  { id: "lokalm04", lokal: lokalm04, active: "" },
+  { id: "lokalm05", lokal: lokalm05, active: "" },
+  { id: "lokalm06", lokal: lokalm06, active: "" },
+  { id: "lokalm07", lokal: lokalm07, active: "" },
+  { id: "lokalm08", lokal: lokalm08, active: "" },
+  { id: "lokalm11", lokal: lokalm11, active: "" },
+  { id: "lokalm12", lokal: lokalm12, active: "" },
+  { id: "lokalm13", lokal: lokalm13, active: "" },
+  { id: "lokalm14", lokal: lokalm14, active: "" },
+  { id: "lokalm15", lokal: lokalm15, active: "" },
+  { id: "lokalm16", lokal: lokalm16, active: "" },
+  { id: "lokalm17", lokal: lokalm17, active: "" },
+  { id: "lokalm18", lokal: lokalm18, active: "" },
+  { id: "lokalm19", lokal: lokalm19, active: "" },
+  { id: "lokalm21", lokal: lokalm21, active: "" },
+  { id: "lokalm22", lokal: lokalm22, active: "" },
+  { id: "lokalm23", lokal: lokalm23, active: "" },
+  { id: "lokalm24", lokal: lokalm24, active: "" },
+  { id: "lokalm25", lokal: lokalm25, active: "" },
+  { id: "lokalm26", lokal: lokalm26, active: "" },
+  { id: "lokalm27", lokal: lokalm27, active: "" },
+  { id: "lokalm28", lokal: lokalm28, active: "" },
+  { id: "lokalm29", lokal: lokalm29, active: "" },
+  { id: "lokalm31", lokal: lokalm31, active: "" },
+  { id: "lokalm32", lokal: lokalm32, active: "" },
+  { id: "lokalm33", lokal: lokalm33, active: "" },
+  { id: "lokalm34", lokal: lokalm34, active: "" },
+  { id: "lokalm35", lokal: lokalm35, active: "" },
+  { id: "lokalm36", lokal: lokalm36, active: "" },
+  { id: "lokalm37", lokal: lokalm37, active: "" },
+];
 
 const Read = () => {
- 
-const [info, setInfo] = useState([]);
-useEffect(() => {
-  setTimeout(() => {
-    Fetchdata();
-  }, 1); //miliseconds
-}, []);
-//  window.addEventListener("load", () => {
-//     Fetchdata();
-//   });
- 
- 
+  const [info, setInfo] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      Fetchdata();
+    }, 1); //miliseconds
+  }, []);
+  //  window.addEventListener("load", () => {
+  //     Fetchdata();
+  //   });
+
   // Fetch the required data using the get() method
-  const Fetchdata = async() => {
-   await db.collection("pawla")
+  const Fetchdata = async () => {
+    await db
+      .collection("pawla")
       .get()
       .then((querySnapshot) => {
         // Loop through the data and store
@@ -105,134 +101,162 @@ useEffect(() => {
 
   const items = info.map((data) => {
     if (data.DOSTEPNOSC == "DOSTĘPNY")
-    return (
-      <Row className="hoverable green-text textMobile">
-         <Col className="tabela" s={2}>
-          {data.LOKAL}
-        </Col>
-        <Col className="tabela" s={2}>
-          {data.METRAZ}
-        </Col>
-        <Col className="tabela" s={1}>
-          {data.PIETRO}
-        </Col>
-        <Col className="tabela" s={2}>
-          {data.CENA}
-        </Col>
-        <Col className="tabela" s={4}>
-          {data.DOSTEPNOSC}
-        </Col>
-        
-      </Row>
-    )
+      return (
+        <Row className="hoverable green-text textMobile">
+          <Col className="tabela" s={2}>
+            {data.LOKAL}
+          </Col>
+          <Col className="tabela" s={2}>
+            {data.METRAZ}
+          </Col>
+          <Col className="tabela" s={1}>
+            {data.PIETRO}
+          </Col>
+          <Col className="tabela" s={2}>
+            {data.CENA}
+          </Col>
+          <Col className="tabela" s={4}>
+            {data.DOSTEPNOSC}
+          </Col>
+        </Row>
+      );
     else
-    return (
-      <Row className="hoverable red-text textMobile">
-        <Col className="tabela" s={2}>
-          {data.LOKAL}
-        </Col>
-        <Col className="tabela" s={2}>
-          {data.METRAZ}
-        </Col>
-        <Col className="tabela" s={1}>
-          {data.PIETRO}
-        </Col>
-        <Col className="tabela" s={2}>
-          {data.CENA}
-        </Col>
-        <Col className="tabela" s={4}>
-          {data.DOSTEPNOSC}
-        </Col>
-      
-      </Row>
-    );
+      return (
+        <Row className="hoverable red-text textMobile">
+          <Col className="tabela" s={2}>
+            {data.LOKAL}
+          </Col>
+          <Col className="tabela" s={2}>
+            {data.METRAZ}
+          </Col>
+          <Col className="tabela" s={1}>
+            {data.PIETRO}
+          </Col>
+          <Col className="tabela" s={2}>
+            {data.CENA}
+          </Col>
+          <Col className="tabela" s={4}>
+            {data.DOSTEPNOSC}
+          </Col>
+        </Row>
+      );
   });
   const items2 = Lista.map((data) => {
     return (
       <Row>
-        <Col className="tabela" s={12}>
-        
-   
-      <Modal
-        className="modalid"
-        actions={[
-          <Button flat modal="close" node="button" small waves="green">
-            Zamknij
-          </Button>,
-        ]}
-        bottomSheet={false}
-        fixedFooter={false}
-        header={"Rzut "}
-        id={data.id}
-        open={false}
-        options={{
-          dismissible: true,
-          endingTop: "10%",
-          inDuration: 250,
-          onCloseEnd: null,
-          onCloseStart: null,
-          onOpenEnd: null,
-          onOpenStart: null,
-          opacity: 0.5,
-          outDuration: 250,
-          preventScrolling: true,
-          startingTop: "4%",
-        }}
-        trigger={
-          <Button  className="red" node="button" small>
-            Rzut
-          </Button>
-        }
-      >
-        <Row>
-          <Divider /> <img src={data.lokal} width="100%" />
-        </Row>
-      </Modal>
- 
+        <Col className="tabela hide-on-small-only" s={12}>
+          <Modal
+            className="modalid"
+            actions={[
+              <Button flat modal="close" node="button" small waves="green">
+                Zamknij
+              </Button>,
+            ]}
+            bottomSheet={false}
+            fixedFooter={false}
+            header={"Rzut "}
+            id={data.id}
+            open={false}
+            options={{
+              dismissible: true,
+              endingTop: "10%",
+              inDuration: 250,
+              onCloseEnd: null,
+              onCloseStart: null,
+              onOpenEnd: null,
+              onOpenStart: null,
+              opacity: 0.5,
+              outDuration: 250,
+              preventScrolling: true,
+              startingTop: "4%",
+            }}
+            trigger={
+              <Button className="red" node="button" small>
+                Rzut
+              </Button>
+            }
+          >
+            <Row>
+              <Divider /> <img src={data.lokal} width="100%" />
+            </Row>
+          </Modal>
+        </Col>
+        <Col className="tabela hide-on-med-and-up" s={12}>
+          <Modal
+            className="modalid"
+            actions={[
+              <Button flat modal="close" node="button" small waves="green">
+                Zamknij
+              </Button>,
+            ]}
+            bottomSheet={false}
+            fixedFooter={false}
+            header={"Rzut "}
+            id={data.id}
+            open={false}
+            options={{
+              dismissible: true,
+              endingTop: "10%",
+              inDuration: 250,
+              onCloseEnd: null,
+              onCloseStart: null,
+              onOpenEnd: null,
+              onOpenStart: null,
+              opacity: 0.5,
+              outDuration: 250,
+              preventScrolling: true,
+              startingTop: "4%",
+            }}
+            trigger={
+              <p className="red white-text" node="button" small>
+                Rzut
+              </p>
+            }
+          >
+            <Row>
+              <Divider /> <img src={data.lokal} width="100%" />
+            </Row>
+          </Modal>
         </Col>
       </Row>
     );
   });
   return (
-
-
-
-
-
-
-
-
-
     <div className="">
-    <Row className="white textMobile pawla12">
-      <Col s={10}>
-        <Row>
-          <Col className="tabela" s={2}>
-            LOKAL
-          </Col>
-          <Col className="tabela" s={2}>
-            M2
-          </Col>
-          <Col className="tabela hide-on-med-and-up" s={1}>
-            PIĘ<br/>TRO
-          </Col>
-          <Col className="tabela hide-on-small-only" s={1}>
-            PIĘTRO
-          </Col>
-          <Col className="tabela" s={2}>
-            CENA
-          </Col>
+      <Row className="white textMobile pawla12">
+        <Col s={10}>
+          <Row>
+            <Col className="tabela" s={2}>
+              LOKAL
+            </Col>
+            <Col className="tabela" s={2}>
+              M2
+            </Col>
+            <Col className="tabela hide-on-med-and-up" s={1}>
+              PIĘ
+              <br />
+              TRO
+            </Col>
+            <Col className="tabela hide-on-small-only" s={1}>
+              PIĘTRO
+            </Col>
+            <Col className="tabela" s={2}>
+              CENA
+            </Col>
 
-          <Col className="tabela" s={4}>
-            DOSTĘPNOSC
-          </Col>
-        </Row>
-        {items}
-      </Col>
-      <Col s={2}>
-        Rzut <br/><Row/><Row/>
-        {items2}</Col>
-    </Row>
+            <Col className="tabela" s={4}>
+              DOSTĘPNOSC
+            </Col>
+          </Row>
+          {items}
+        </Col>
+        <Col s={2}>
+          Rzut <br />
+          <Row /> <Row />
+          
+          {items2}
+        </Col>
+      </Row>
     </div>
   );
 };
