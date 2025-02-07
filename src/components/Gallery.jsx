b876def4c4180994c347092f25a0c983da4fae2d
cholujdesign <79589369+cholujdesign@users.noreply.github.com>
2025-02-07 14:59:51 +0100

    h

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -47,172 +47,155 @@ export default class Gallery extends React.Component {
         <br />
         <Row /><div className="standard"></div>
         <Row>
-        <Col s={12} m={6}>
-          <MediaBox
-            className="z-depth-3"
-            id="MediaBox_1"
-            options={{
-              inDuration: 275,
-              onCloseEnd: null,
-              onCloseStart: null,
-              onOpenEnd: null,
-              onOpenStart: null,
-              outDuration: 200,
-            }}
-          >
-            <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294513970_5459907027399205_4461154066782848590_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Q2hwhnNMoesAX9PUaYc&_nc_ht=scontent.flcj1-1.fna&oh=00_AT93HYM7NfpGoWebeEw1MrSlCxaHzYq-uBLr0oGnnLJd7Q&oe=62DBB30A" src={img19} width="100%" />
-          </MediaBox>
+          <Col s={12} m={6}>
+            <MediaBox
+              className="z-depth-3"
+              id="MediaBox_1"
+              options={{
+                inDuration: 275,
+                onCloseEnd: null,
+                onCloseStart: null,
+                onOpenEnd: null,
+                onOpenStart: null,
+                outDuration: 200,
+              }}
+            >
+              <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294513970_5459907027399205_4461154066782848590_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Q2hwhnNMoesAX9PUaYc&_nc_ht=scontent.flcj1-1.fna&oh=00_AT93HYM7NfpGoWebeEw1MrSlCxaHzYq-uBLr0oGnnLJd7Q&oe=62DBB30A" src={img19} width="100%" />
+            </MediaBox>
 
-        </Col>
-        <Col s={12} m={6}>
-          <MediaBox
-            className="z-depth-3"
-            id="MediaBox_1"
-            options={{
-              inDuration: 275,
-              onCloseEnd: null,
-              onCloseStart: null,
-              onOpenEnd: null,
-              onOpenStart: null,
-              outDuration: 200,
-            }}
-          >
-            <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294513970_5459907027399205_4461154066782848590_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Q2hwhnNMoesAX9PUaYc&_nc_ht=scontent.flcj1-1.fna&oh=00_AT93HYM7NfpGoWebeEw1MrSlCxaHzYq-uBLr0oGnnLJd7Q&oe=62DBB30A" src={img20} width="100%" />
-          </MediaBox>
+          </Col>
+          <Col s={12} m={6}>
+            <MediaBox
+              className="z-depth-3"
+              id="MediaBox_1"
+              options={{
+                inDuration: 275,
+                onCloseEnd: null,
+                onCloseStart: null,
+                onOpenEnd: null,
+                onOpenStart: null,
+                outDuration: 200,
+              }}
+            >
+              <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294513970_5459907027399205_4461154066782848590_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Q2hwhnNMoesAX9PUaYc&_nc_ht=scontent.flcj1-1.fna&oh=00_AT93HYM7NfpGoWebeEw1MrSlCxaHzYq-uBLr0oGnnLJd7Q&oe=62DBB30A" src={img20} width="100%" />
+            </MediaBox>
 
 
-        </Col>
+          </Col>
 
 
         </Row><Row>
-        <Col s={12} m={6}>
-          <MediaBox
-            className="z-depth-3"
-            id="MediaBox_1"
-            options={{
-              inDuration: 275,
-              onCloseEnd: null,
-              onCloseStart: null,
-              onOpenEnd: null,
-              onOpenStart: null,
-              outDuration: 200,
-            }}
-          >
-            <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294513970_5459907027399205_4461154066782848590_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Q2hwhnNMoesAX9PUaYc&_nc_ht=scontent.flcj1-1.fna&oh=00_AT93HYM7NfpGoWebeEw1MrSlCxaHzYq-uBLr0oGnnLJd7Q&oe=62DBB30A" src={scene8} width="100%" />
-          </MediaBox>
+          <Col s={12} m={6}>
+            <MediaBox
+              className="z-depth-3"
+              id="MediaBox_1"
+              options={{
+                inDuration: 275,
+                onCloseEnd: null,
+                onCloseStart: null,
+                onOpenEnd: null,
+                onOpenStart: null,
+                outDuration: 200,
+              }}
+            >
+              <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294513970_5459907027399205_4461154066782848590_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Q2hwhnNMoesAX9PUaYc&_nc_ht=scontent.flcj1-1.fna&oh=00_AT93HYM7NfpGoWebeEw1MrSlCxaHzYq-uBLr0oGnnLJd7Q&oe=62DBB30A" src={scene9} width="100%" />
+            </MediaBox>
 
-        </Col>
-        <Col s={12} m={6}>
-          <MediaBox
-            className="z-depth-3"
-            id="MediaBox_1"
-            options={{
-              inDuration: 275,
-              onCloseEnd: null,
-              onCloseStart: null,
-              onOpenEnd: null,
-              onOpenStart: null,
-              outDuration: 200,
-            }}
-          >
-            <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294513970_5459907027399205_4461154066782848590_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Q2hwhnNMoesAX9PUaYc&_nc_ht=scontent.flcj1-1.fna&oh=00_AT93HYM7NfpGoWebeEw1MrSlCxaHzYq-uBLr0oGnnLJd7Q&oe=62DBB30A" src={scene7} width="100%" />
-          </MediaBox>
+          </Col>
+          <Col s={12} m={6}>
+            <MediaBox
+              className="z-depth-3"
+              id="MediaBox_1"
+              options={{
+                inDuration: 275,
+                onCloseEnd: null,
+                onCloseStart: null,
+                onOpenEnd: null,
+                onOpenStart: null,
+                outDuration: 200,
+              }}
+            >
+              <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294513970_5459907027399205_4461154066782848590_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Q2hwhnNMoesAX9PUaYc&_nc_ht=scontent.flcj1-1.fna&oh=00_AT93HYM7NfpGoWebeEw1MrSlCxaHzYq-uBLr0oGnnLJd7Q&oe=62DBB30A" src={scene7} width="100%" />
+            </MediaBox>
 
-        </Col>
+          </Col>
         </Row><Row>
-        <Col s={12} m={6}>
-          <MediaBox
-            className="z-depth-3"
-            id="MediaBox_6"
-            options={{
-              inDuration: 275,
-              onCloseEnd: null,
-              onCloseStart: null,
-              onOpenEnd: null,
-              onOpenStart: null,
-              outDuration: 200,
-            }}
-          >
-            <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294285645_5459907120732529_7665346751386665222_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=F5hvg0x1KGYAX8EMmup&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-n2xG634dG3_tg96lx8AaM_ei7oUDvI6PCOiBlNM9DSA&oe=62DB3163" src={img21} width="100%" />
-          </MediaBox>
+          <Col s={12} m={6}>
+            <MediaBox
+              className="z-depth-3"
+              id="MediaBox_6"
+              options={{
+                inDuration: 275,
+                onCloseEnd: null,
+                onCloseStart: null,
+                onOpenEnd: null,
+                onOpenStart: null,
+                outDuration: 200,
+              }}
+            >
+              <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294285645_5459907120732529_7665346751386665222_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=F5hvg0x1KGYAX8EMmup&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-n2xG634dG3_tg96lx8AaM_ei7oUDvI6PCOiBlNM9DSA&oe=62DB3163" src={img21} width="100%" />
+            </MediaBox>
 
-        </Col>
+          </Col>
 
-        <Col s={12} m={6}>
-          <MediaBox
-            className="z-depth-3"
-            id="MediaBox_1"
-            options={{
-              inDuration: 275,
-              onCloseEnd: null,
-              onCloseStart: null,
-              onOpenEnd: null,
-              onOpenStart: null,
-              outDuration: 200,
-            }}
-          >
-            <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294513970_5459907027399205_4461154066782848590_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Q2hwhnNMoesAX9PUaYc&_nc_ht=scontent.flcj1-1.fna&oh=00_AT93HYM7NfpGoWebeEw1MrSlCxaHzYq-uBLr0oGnnLJd7Q&oe=62DBB30A" src={scene9} width="100%" />
-          </MediaBox>
-
-        </Col>
-
-</Row>
-
-
-<Row>
-
-        <Col s={12} m={6}>
-          <MediaBox
-            className="z-depth-3"
-            id="MediaBox_6"
-            options={{
-              inDuration: 275,
-              onCloseEnd: null,
-              onCloseStart: null,
-              onOpenEnd: null,
-              onOpenStart: null,
-              outDuration: 200,
-            }}
-          >
-            <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294285645_5459907120732529_7665346751386665222_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=F5hvg0x1KGYAX8EMmup&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-n2xG634dG3_tg96lx8AaM_ei7oUDvI6PCOiBlNM9DSA&oe=62DB3163" src={imgh1} width="100%" />
-          </MediaBox>
-
-        </Col>
-        <Col s={12} m={6}>
-          <MediaBox
-            className="z-depth-3"
-            id="MediaBox_6"
-            options={{
-              inDuration: 275,
-              onCloseEnd: null,
-              onCloseStart: null,
-              onOpenEnd: null,
-              onOpenStart: null,
-              outDuration: 200,
-            }}
-          >
-            <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294285645_5459907120732529_7665346751386665222_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=F5hvg0x1KGYAX8EMmup&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-n2xG634dG3_tg96lx8AaM_ei7oUDvI6PCOiBlNM9DSA&oe=62DB3163" src={imgh4} width="100%" />
-          </MediaBox>
-
-        </Col>
+          <Col s={12} m={6}>
+            <MediaBox
+              className="z-depth-3"
+              id="MediaBox_6"
+              options={{
+                inDuration: 275,
+                onCloseEnd: null,
+                onCloseStart: null,
+                onOpenEnd: null,
+                onOpenStart: null,
+                outDuration: 200,
+              }}
+            >
+              <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294285645_5459907120732529_7665346751386665222_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=F5hvg0x1KGYAX8EMmup&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-n2xG634dG3_tg96lx8AaM_ei7oUDvI6PCOiBlNM9DSA&oe=62DB3163" src={imgh4} width="100%" />
+            </MediaBox>
+          </Col>
         </Row>
-        <Col s={12} m={6}>
-          <MediaBox
-            className="z-depth-3"
-            id="MediaBox_6"
-            options={{
-              inDuration: 275,
-              onCloseEnd: null,
-              onCloseStart: null,
-              onOpenEnd: null,
-              onOpenStart: null,
-              outDuration: 200,
-            }}
-          >
-            <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294285645_5459907120732529_7665346751386665222_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=F5hvg0x1KGYAX8EMmup&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-n2xG634dG3_tg96lx8AaM_ei7oUDvI6PCOiBlNM9DSA&oe=62DB3163" src={imgh2} width="100%" />
-          </MediaBox>
 
-        </Col>
+
+        <Row>
+
+          <Col s={12} m={6}>
+            <MediaBox
+              className="z-depth-3"
+              id="MediaBox_6"
+              options={{
+                inDuration: 275,
+                onCloseEnd: null,
+                onCloseStart: null,
+                onOpenEnd: null,
+                onOpenStart: null,
+                outDuration: 200,
+              }}
+            >
+              <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294285645_5459907120732529_7665346751386665222_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=F5hvg0x1KGYAX8EMmup&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-n2xG634dG3_tg96lx8AaM_ei7oUDvI6PCOiBlNM9DSA&oe=62DB3163" src={imgh1} width="100%" />
+            </MediaBox>
+
+          </Col>
+
+          <Col s={12} m={6}>
+            <MediaBox
+              className="z-depth-3"
+              id="MediaBox_6"
+              options={{
+                inDuration: 275,
+                onCloseEnd: null,
+                onCloseStart: null,
+                onOpenEnd: null,
+                onOpenStart: null,
+                outDuration: 200,
+              }}
+            >
+              <img alt="https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294285645_5459907120732529_7665346751386665222_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=F5hvg0x1KGYAX8EMmup&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-n2xG634dG3_tg96lx8AaM_ei7oUDvI6PCOiBlNM9DSA&oe=62DB3163" src={imgh2} width="100%" />
+            </MediaBox>
+
+          </Col>
+        </Row>
+
 
 
 
